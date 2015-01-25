cf15876acd447b2815a22ff7df1081e12011d08d
Derek Van Dyke <haoled@gmail.com>
2015-01-25 10:23:41 -0800

    Complete - specs pass

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -2,9 +2,33 @@
 // var getElementsByClassName = function (className) {
 //   return document.getElementsByClassName(className);
 // };
-
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className, node){
   // your code here
+  var classes = className.split(" ");
+  var results = [];
+
+  var traverse = function (nodes){
+  	if(nodes !== undefined){
+  		var classList = nodes.classList;
+  		if (nodes.length){
+  			for (var i = 0, length = nodes.length; i<length; i++){
+  				if(nodes[i].classList !== undefined){
+  					traverse(nodes[i]);
+  				}
+  			}
+  		}
+  		else {
+  			if(classList){
+  				if(classList.contains(className)){
+  					results.push(nodes);
+  				}
+  				traverse(nodes.childNodes);
+  			}
+  		}
+  	}
+
+  };
+  traverse(document.body);
+  return results;
 };
