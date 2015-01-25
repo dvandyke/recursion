a7d48be93b8a7c9faec0123066b383c3a90cc494
Derek Van Dyke <haoled@gmail.com>
2015-01-25 10:25:18 -0800

    Complete - specs pass

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,23 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //your code goes here
+    var sOutput = "";
+    if(obj instanceof Object){
+  	if(obj.constructor === Array){
+  		for (var nId = 0; nId < obj.length; sOutput += stringifyJSON(obj[nId]) + ",", nId++);
+  			return "[" + sOutput.substr(0, sOutput.length - 1) + "]";
+  	}
+  	if(obj.toString	!== Object.prototype.toString){
+  		return "\"" + obj.toString().replace(/"/g, "\\$&") + "\"";
+  	}
+  	for (var sProp in obj){
+      if(obj[sProp] instanceof Function || undefined){
+        return "{}";
+      }
+  		sOutput += "\"" + sProp.replace(/"/g, "\\$&") + "\":" + stringifyJSON(obj[sProp]) + ",";
+  	}
+  	return "{" + sOutput.substr(0, sOutput.length -1) + "}";
+  }
+  return typeof obj === "string" ? "\"" + obj.replace(/"/g, "\\$&") + "\"" : String(obj);
 };
